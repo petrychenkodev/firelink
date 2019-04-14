f14d9bd3dc833ff91e2c5d89b670037c1f453f07
petrychenkodev <panleshapan@gmail.com>
2019-04-14 20:55:45 +0300

    JS for EI11

--- js/main.js
+++ js/main.js
@@ -1,71 +1,103 @@
-function removeClass(block, active){
-	const parentBlock = block;
-	for(let i = 0; i < parentBlock.children.length; i++){
-		event.target.nodeName == 'LI' && (
-		parentBlock.children[i].classList.contains(active) &&
-		parentBlock.children[i].classList.remove(active));
-	}
-}
-function addClass(className){
-	event.target.nodeName == 'LI' &&
-	event.target.classList.add(className);
+"use strict";
+
+function removeClass(block, active) {
+  var parentBlock = block;
+
+  for (var i = 0; i < parentBlock.children.length; i++) {
+    event.target.nodeName == 'LI' && parentBlock.children[i].classList.contains(active) && parentBlock.children[i].classList.remove(active);
+  }
 }
 
-const langBlock = document.querySelector('.choose__lang');
-langBlock.addEventListener('click', ()=>{
-	removeClass(langBlock, 'lang__active');
-	addClass('lang__active');
-});
-
-const currBlock = document.querySelector('.choose__curr');
-currBlock.addEventListener('click', ()=>{
-	removeClass(currBlock, 'curr__active');
-	addClass('curr__active');
-});
-
-const navBlock = document.querySelector('.navigation__list');
-navBlock.addEventListener('click', ()=>{
-	removeClass(navBlock, 'navigation--active');
-	addClass('navigation--active');
-});
-
-function slideImg (slider){
-	const sliderWidth = slider.offsetWidth;
-	const maxWidth = movable.children.length - 1;
-	if(event.target.classList.contains('slider__right')){
-		width += sliderWidth;
-		if(width > sliderWidth * maxWidth) {width = sliderWidth * maxWidth};
-		movable.style.cssText = `margin-left : -${width}px`;
-		if(count >= pointsCount-1){count = pointsCount-1}else{count++};
-	}
-	if(event.target.classList.contains('slider__left')){
-		width -= sliderWidth;
-		if(width < 0) {width = 0};
-		movable.style.cssText = `margin-left : -${width}px`;
-		if(count <= 0){count = 0}else{count--};
-	}
-	points.innerHTML = '';
-	for (let i = 0; i < pointsCount; i++){
-		const li = document.createElement('li');
-		if(i==count){li.classList.add('slider__active')};
-		points.appendChild(li);
-	}
+function addClass(className) {
+  event.target.nodeName == 'LI' && event.target.classList.add(className);
 }
-let width = 0;
-let count = 0;
 
-const slider = document.querySelector('.slider__inside');
-slider.addEventListener('click', ()=>{
-	slideImg (slider);
+var langBlock = document.querySelector('.choose__lang');
+langBlock.addEventListener('click', function () {
+  removeClass(langBlock, 'lang__active');
+  addClass('lang__active');
+});
+var currBlock = document.querySelector('.choose__curr');
+currBlock.addEventListener('click', function () {
+  removeClass(currBlock, 'curr__active');
+  addClass('curr__active');
+});
+var navBlock = document.querySelector('.navigation__list');
+navBlock.addEventListener('click', function () {
+  removeClass(navBlock, 'navigation--active');
+  addClass('navigation--active');
 });
 
-const points = slider.querySelector('.slider__pointers');
-const movable = slider.querySelector('.slider__content');
-let pointsCount = movable.children.length;
-let pointersArr = [];
-for (let i = 0; i < pointsCount; i++){
-	const li = document.createElement('li');
-	points.appendChild(li);
-	pointersArr.push(li);
-	pointersArr[count].classList.add('slider__active');
+function slideImg(slider) {
+  var sliderWidth = slider.offsetWidth;
+  var maxWidth = movable.children.length - 1;
+
+  if (event.target.classList.contains('slider__right')) {
+    width += sliderWidth;
+
+    if (width > sliderWidth * maxWidth) {
+      width = sliderWidth * maxWidth;
+    }
+
+    ;
+    movable.style.cssText = "margin-left : -".concat(width, "px");
+
+    if (count >= pointsCount - 1) {
+      count = pointsCount - 1;
+    } else {
+      count++;
+    }
+
+    ;
+  }
+
+  if (event.target.classList.contains('slider__left')) {
+    width -= sliderWidth;
+
+    if (width < 0) {
+      width = 0;
+    }
+
+    ;
+    movable.style.cssText = "margin-left : -".concat(width, "px");
+
+    if (count <= 0) {
+      count = 0;
+    } else {
+      count--;
+    }
+
+    ;
+  }
+
+  points.innerHTML = '';
+
+  for (var i = 0; i < pointsCount; i++) {
+    var li = document.createElement('li');
+
+    if (i == count) {
+      li.classList.add('slider__active');
+    }
+
+    ;
+    points.appendChild(li);
+  }
+}
+
+var width = 0;
+var count = 0;
+var slider = document.querySelector('.slider__inside');
+slider.addEventListener('click', function () {
+  slideImg(slider);
+});
+var points = slider.querySelector('.slider__pointers');
+var movable = slider.querySelector('.slider__content');
+var pointsCount = movable.children.length;
+var pointersArr = [];
+
+for (var i = 0; i < pointsCount; i++) {
+  var li = document.createElement('li');
+  points.appendChild(li);
+  pointersArr.push(li);
+  pointersArr[count].classList.add('slider__active');
 }
