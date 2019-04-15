3f15f3054aa57931900ffca02e995d8f4299d21a
petrychenkodev <panleshapan@gmail.com>
2019-04-15 10:05:32 +0300

    done

--- js/main.js
+++ js/main.js
@@ -101,3 +101,25 @@ for (var i = 0; i < pointsCount; i++) {
   pointersArr.push(li);
   pointersArr[count].classList.add('slider__active');
 }
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+var login = _toConsumableArray(document.querySelectorAll('.login--start'));
+
+var popup = document.querySelector('.popup');
+login.forEach(function (el) {
+  el.addEventListener('click', function () {
+    popup.classList.add('popup--active');
+  });
+});
+popup.addEventListener('click', function (e) {
+  if (!e.target.classList.contains('popup__dnc')) {
+    popup.classList.remove('popup--active');
+  }
+});
